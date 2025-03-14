d95c893c2918c044451bc6bfb223fec21cf51e57
AStepchenkooo <stepchenko.ant@gmail.com>
2025-03-15 01:18:02 +0200

    +class sentence

--- S2L2/Program.cs
+++ S2L2/Program.cs
@@ -1,2 +1,75 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using S2L2;
+
+bool testing = true;
+Sentence s = new Sentence();
+while (testing)
+{
+    Console.Write($"Зараз речення таке: {s.sentence}\n" +
+        $"1 - додати слово в кінець\n2 - видалити слово \n" +
+        $"3 - вставити слово за індексом\n4 - отримати кількість літер\n" +
+        $"5 - отримати кількість слів\n6 - отримамти найдовше слово\n" +
+        $"7 - отримати найкоротше слово\n8 - перевірити чи є в реченні слово\n" +
+        $"9 - отримати слово за індексом\n10 - порівняти з іншим реченням\n" +
+        $"\"-\" - закінчити з даним реченням\n" +
+        $"Введіть наступну дію: ");
+    string answer = Console.ReadLine();
+    int action=0;
+    if (answer == "-")
+    {
+        testing = false;
+        break;
+    }
+    else action = int.Parse(answer);
+    switch (action)
+    {
+        case 1:
+            Console.Write("Введіть слово: ");
+            s.Add(Console.ReadLine());
+            break;
+        case 2:
+            Console.Write("Введіть слово: ");
+            s.Remove(Console.ReadLine());
+            break;
+        case 3:
+            Console.Write("Введіть індекс:");
+            int index1 = int.Parse(Console.ReadLine());
+            Console.Write("Введіть слово: ");
+            s.InsertWord(index1, Console.ReadLine());
+            break;
+        case 4:
+            Console.WriteLine($"Кількість літер: {s.LetterCount}");
+            break;
+        case 5:
+            Console.WriteLine($"Кількість слів: {s.WordCount}");
+            break;
+        case 6:
+            Console.WriteLine($"Найдовше слово: {s.LongestWord}");
+            break;
+        case 7:
+            Console.WriteLine($"Найкоротше слово: {s.ShortestWord}");
+            break;
+        case 8:
+            Console.Write($"Введіть слово: ");
+            if (s.IsContainWord(Console.ReadLine())) 
+                Console.WriteLine("Речення містить дане слово");
+            else
+                Console.WriteLine("Речення не містить дане слово");
+            break;
+        case 9:
+            Console.Write("Введіть індекс:");
+            int index2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(s.WordByIndex(index2));
+            break;
+        case 10:
+            Console.Write("Введіть речення: ");
+            if (s.IsSentencesEqual(Console.ReadLine()))
+                Console.WriteLine("Речення однакові");
+            else
+                Console.WriteLine("Речення різні");
+            break;
+        default:
+            break;
+    }
+    Console.ReadKey();
+    Console.Clear();
+}
---
+++ S2L2/sentence.cs
@@ -0,0 +1,65 @@
+﻿namespace S2L2
+{
+    class Sentence
+    {
+        public string sentence;
+
+        public void Add(string word)
+        {
+            sentence += " " + word;
+        }
+        public void Remove(string word)
+        {
+            var words = sentence.Split(' ').ToList();
+            if (words.Contains(word))
+                words.Remove(word);
+            else
+                Console.WriteLine("Речення не містить дане слово");
+            sentence = string.Join(' ', words);
+
+        }
+        public void InsertWord(int index, string word)
+        {
+            var words = sentence.Split(' ').ToList();
+            if (index >= 0 && index < words.Count)
+            {
+                words.Insert(index, word);
+                sentence = string.Join(' ', words);
+            }
+        }
+        public int LetterCount()
+        {
+            return sentence.Replace(" ", "").Length;
+        }
+        public int WordCount()
+        {
+            var words = sentence.Split(' ').ToList();
+            return words.Count;
+        }
+        public string LongestWord()
+        {
+            return sentence.Split(' ').ToList().OrderByDescending(i => i.Length).First();
+        }
+        public string ShortestWord()
+        {
+            return sentence.Split(' ').ToList().OrderBy(i => i.Length).First();
+        }
+        public bool IsContainWord(string word)
+        {
+            return sentence.Split(' ').Contains(word);
+        }
+        public string WordByIndex(int index)
+        {
+            var words = sentence.Split(' ').ToList();
+            if (index >= 0 && index < words.Count)
+                return words[index];
+            else
+                Console.WriteLine("Рядок не містить слова за даним індексом");
+            return null;
+        }
+        public bool IsSentencesEqual(string sentence2)
+        {
+            return sentence == sentence2;
+        }
+    }
+}
